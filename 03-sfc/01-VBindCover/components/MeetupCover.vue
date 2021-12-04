3b095aa854ee6892be3d6960c3bcc2e59dec614c
Andrej Glukhov <glukhov.and@gmail.com>
2021-12-05 00:24:08 +1000

    03-sfc 01-VBindCover

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+  <div class="meetup-cover" >
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+  props: {
+    title: {
+      type: String
+      ,default: ''
+    },
+    image : {
+      type: String
+      ,default: ''
+    },
+  },
+  computed: {
+    image_url() {
+      return "url('" + this.image + "')";
+    },
+  },
 };
+
 </script>
 
 <style scoped>
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),  v-bind( image_url );  
   display: flex;
   flex-direction: column;
   align-items: center;
