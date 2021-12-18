2bed15f511af9dcaec16a39881a9350a544e7b0c
Andrej Glukhov <glukhov.and@gmail.com>
2021-12-19 00:43:09 +1000

    05-vue-router\01-AuthPages

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,6 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import PageIndex from '../views/PageIndex.vue';
+import PageLogin from '../views/PageLogin.vue';
+import PageRegister from '../views/PageRegister.vue';
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Start',
+      component: PageIndex,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: PageLogin,
+    },
+    {
+      path: '/register',
+      name: 'Registr',
+      component: PageRegister,
+    },
+  ],
 });
--- 05-vue-router/01-AuthPages/views/PageRegister.vue
+++ 05-vue-router/01-AuthPages/views/PageRegister.vue
@@ -31,9 +31,9 @@
           </label>
         </ui-form-group>
         <div class="form__buttons">
-          <button type="submit" class="button button_primary">Зарегистрироваться</button>
+          <button @click="handleSubmit" type="submit" class="button button_primary">Зарегистрироваться</button>
         </div>
-        <div class="form__append">Уже есть аккаунт? <a href="/login" class="link">Войдите</a></div>
+        <div class="form__append">Уже есть аккаунт? <router-link :to="'/login'" class="link">Войдите</router-link></div>
       </form>
     </ui-container>
   </div>
@@ -54,6 +54,8 @@ export default {
   methods: {
     handleSubmit() {
       // Требуется обработать сабмит формы
+      this.$router.push({name: 'Login' });
+
     },
   },
 };
