8c74e0de591dd27fc9f0af8a93eaa144b082383b
Andrej Glukhov <glukhov.and@gmail.com>
2021-12-21 23:01:24 +1000

    05-vue-router\03-ScrollBehavior

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -3,6 +3,29 @@ import { createRouter, createWebHistory } from 'vue-router';
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
 
+  scrollBehavior(to, from, savedPosition) {
+/*
+- При переходе по умолчанию положение должно быть в начале страницы
+- При переходе назад/вперёд с `savedPosition` положение на странице должно оставаться на `savedPosition`;
+- При переходе на маршрут с `hash` положение на странице должно определяться блоком с идентификатором `hash`;
+- При переходе между маршрутами c истинными meta свойствами saveScrollPosition у обоих маршрутов положение на странице
+  не должно изменяться.
+*/ 
+   
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+      }
+    } else if (to.meta.saveScrollPosition && from.meta.saveScrollPosition ) {
+      return false;
+    } else {
+      return {
+        left: 0, top: 0
+      }
+    }
+  },    
   routes: [
     {
       path: '/',
